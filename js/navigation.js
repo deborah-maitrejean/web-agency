513eced0e9235ac1adacd1bed07ea3ac49d2ef83
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-14 16:15:57 +0200

    create fluid navigation js script

---
+++ js/navigation.js
@@ -0,0 +1,10 @@
+/*---------- Navigation fluide ----------*/
+$(document).ready(function () {
+    $('a.scroll').on('click', function (evt) {
+        evt.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body')
+            .stop()
+            .animate({ scrollTop: $(target).offset().top - 150 }, 1000);
+    });
+});
