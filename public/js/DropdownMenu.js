1541a6ee987ab7331ed6e1ec48afbbf606f5bd82
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-15 20:57:18 +0200

    create dropdown js script

---
+++ public/js/DropdownMenu.js
@@ -0,0 +1,27 @@
+var DropdownMenu = {
+    init: function () {
+        var dropdownContent = document.querySelector('#dropdown-content');
+        var btnOpen = document.getElementById('dropbtn');
+        var btnClose = document.getElementById('dropbtn-close');
+        btnClose.style.display = 'none';
+
+        btnOpen.addEventListener('click', function () {
+            btnOpen.style.display = 'none';
+            btnClose.style.display = 'block';
+            dropdownContent.style.display = 'block';
+        });
+
+        btnClose.addEventListener('click', function () {
+            btnClose.style.display = 'none';
+            btnOpen.style.display = 'block';
+            dropdownContent.style.display = 'none';
+        });
+
+        dropdownContent.addEventListener('click', function () {
+            btnClose.style.display = 'none';
+            btnOpen.style.display = 'block';
+            dropdownContent.style.display = 'none';
+        });
+    },
+};
+DropdownMenu.init();
