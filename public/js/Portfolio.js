9484c36dbc6de5b51ccb019c569f510557a4a47c
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-19 01:10:54 +0200

    update Portfolio js class

--- public/js/Portfolio.js
+++ public/js/Portfolio.js
@@ -1,6 +1,37 @@
-var Portfolio = {
-  init: function () {
+var allWorksLi = document.getElementById('allworks');
+var creativeLi = document.getElementById('creative');
+var corporateLi = document.getElementById('corporate');
+var portfolioLi = document.getElementById('portfolio');
 
-  },
+var creative = document.querySelectorAll('.creative');
+var corporate = document.querySelectorAll('.corporate');
+var portfolio = document.querySelectorAll('.portfolio');
+
+var Portfolio = {
+    init: function () {
+        this.shortPortfolio();
+    },
+    shortPortfolio: function () {
+        allWorksLi.addEventListener('click', function () {
+            creative.style.display = 'block';
+            corporate.style.display = 'block';
+            portfolio.style.display = 'block';
+        });
+        creativeLi.addEventListener('click', function () {
+            creative.style.display = 'block';
+            corporate.style.display = 'none';
+            portfolio.style.display = 'none';
+        });
+        corporateLi.addEventListener('click', function () {
+            corporate.style.display = 'block';
+            creative.style.display = 'none';
+            portfolio.style.display = 'none';
+        });
+        portfolioLi.addEventListener('click', function () {
+            portfolio.style.display = 'block';
+            creative.style.display = 'none';
+            corporate.style.display = 'none';
+        });
+    },
 };
 Portfolio.init();
