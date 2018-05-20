f68abbd6d94b57b4408324cb3f8c059e678a8377
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-20 05:04:01 +0200

    update Portfolio js class

--- public/js/Portfolio.js
+++ public/js/Portfolio.js
@@ -7,6 +7,12 @@ var creative = document.querySelectorAll('.creative');
 var corporate = document.querySelectorAll('.corporate');
 var portfolio = document.querySelectorAll('.portfolio');
 
+var actives = document.querySelectorAll('.active-projects-links');
+var allWorksLink = document.querySelector('#allworks a');
+var creativeLink = document.querySelector('#creative a');
+var corporateLink = document.querySelector('#corporate a');
+var portfolioLink = document.querySelector('#portfolio a');
+
 var Portfolio = {
     init: function () {
         this.shortPortfolio();
@@ -14,21 +20,29 @@ var Portfolio = {
     shortPortfolio: function () {
         allWorksLi.addEventListener('click', function () {
             Portfolio.showElements(creative);
+            Portfolio.activeLink(allWorksLink);
+
             Portfolio.showElements(corporate);
             Portfolio.showElements(portfolio);
         });
         creativeLi.addEventListener('click', function () {
             Portfolio.showElements(creative);
+            Portfolio.activeLink(creativeLink);
+
             Portfolio.hideElements(corporate);
             Portfolio.hideElements(portfolio);
         });
         corporateLi.addEventListener('click', function () {
             Portfolio.showElements(corporate);
+            Portfolio.activeLink(corporateLink);
+
             Portfolio.hideElements(creative);
             Portfolio.hideElements(portfolio);
         });
         portfolioLi.addEventListener('click', function () {
             Portfolio.showElements(portfolio);
+            Portfolio.activeLink(portfolioLink);
+
             Portfolio.hideElements(creative);
             Portfolio.hideElements(corporate);
         });
@@ -48,6 +62,15 @@ var Portfolio = {
                 el.style.display = 'none';
             }
         );
-    }
+    },
+    activeLink: function (param) { /*ici*/
+        [].forEach.call(
+            actives,
+            function (el) {
+                el.classList.remove('active-projects-links');
+            }
+        );
+        param.classList.add('active-projects-links');
+    },
 };
 Portfolio.init();
